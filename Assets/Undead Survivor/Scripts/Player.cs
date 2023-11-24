2e5f877940f09c3301101602ea34b7d65a52f472
choijinhyuck <choijinhyuck@naver.com>
2023-11-24 10:29:02 +0900

    First Commit

---
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public Vector2 inputVector;
+    public Scanner scanner;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        scanner = GetComponent<Scanner>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 nextVec = inputVector * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVector.magnitude);
+
+        if (inputVector.x != 0)
+        {
+            spriteRenderer.flipX = inputVector.x < 0;
+        }
+    }
+    void OnMove(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+    }
+}
---
+++ Assets/Undead Survivor/Scripts/Weapon.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int id;
+    public int prefabId;
+    public float damage;
+    public int count;
+    public float speed;
+
+    float timer;
+    Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+
+    private void Start()
+    {
+        Init();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (id)
+        {
+            case 0:
+                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+                break;
+            default:
+                timer += Time.deltaTime;
+
+                if(timer > speed)
+                {
+                    timer = 0f;
+                    Fire();
+                }
+                break;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            LevelUp(10, 1);
+        }
+    }
+
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if (id == 0) Batch();
+    }
+
+    public void Init()
+    {
+        switch (id)
+        {
+            case 0:
+                speed = 150;
+                Batch();
+                break;
+            default:
+                speed = 0.3f;
+                Fire();
+                break;
+        }
+    }
+
+    void Batch()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Transform bullet;
+
+            if (i < transform.childCount)
+            {
+                bullet = transform.GetChild(i);
+            }
+            else
+            {
+                bullet = GameManager.Instance.pool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+
+
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            Vector3 rotVec = Vector3.forward * 360 * i / count;
+            bullet.Rotate(rotVec);
+            bullet.Translate(bullet.up * 1.5f, Space.World);
+
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1 is Infinity Per.
+        }
+
+    }
+    void Fire()
+    {
+        if (!player.scanner.nearestTarget)
+            return;
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.Instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
+    }
+}
